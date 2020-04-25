f484cb8fc453b86bd82735968b7e24e870330e81
Jack Alen <43927158+lovinglover@users.noreply.github.com>
2020-04-25 17:55:26 +0800

    Add files via upload

---
+++ mainwindow.cpp
@@ -0,0 +1,164 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    connect(ui->pushButton, SIGNAL(released()), this, SLOT(plot()));
+    creat_tree();
+    caught_select();
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::creat_tree()
+{
+    QStandardItemModel *model = new QStandardItemModel();
+    // firstlevel
+    QStandardItem *firstlevel_1 = new QStandardItem("TH");
+    QStandardItem *firstlevel_2 = new QStandardItem("WI");
+    QStandardItem *firstlevel_3 = new QStandardItem("RT");
+    QStandardItem *firstlevel_4 = new QStandardItem("RS");
+    // secondlevel
+    QStandardItem *secendlevel_1_1 = new QStandardItem("cur_temp");
+    QStandardItem *secondlevel_1_2 = new QStandardItem("cur_dew_point_tem");
+    QStandardItem *secondlevel_1_3 = new QStandardItem("cur_rel_hum");
+    QStandardItem *secondlevel_1_4 = new QStandardItem("cur_water_vapor_pre");
+    QStandardItem *secondlevel_2_1 = new QStandardItem("cur_wind_dire");
+    QStandardItem *secondlevel_2_2 = new QStandardItem("cur_wind_speed");
+    QStandardItem *secondlevel_2_3 = new QStandardItem("max_wind_speed/min");
+    QStandardItem *secondlevel_2_4 = new QStandardItem("dire_max_wind_speed/min");
+    QStandardItem *secondlevel_3_1 = new QStandardItem("cur_road_tem");
+    QStandardItem *secondlevel_4_1 = new QStandardItem("cur_snow_th");
+    QStandardItem *secondlevel_4_2 = new QStandardItem("cur_water_th");
+    QStandardItem *secondlevel_4_3 = new QStandardItem("cur_ice_th");
+    QStandardItem *secondlevel_4_4 = new QStandardItem("cur_free_tem");
+    QStandardItem *secondlevel_4_5 = new QStandardItem("cur_snow_mel_con");
+
+    model->appendRow(firstlevel_1);
+    model->appendRow(firstlevel_2);
+    model->appendRow(firstlevel_3);
+    model->appendRow(firstlevel_4);
+
+    firstlevel_1->appendRow(secendlevel_1_1);
+    firstlevel_1->appendRow(secondlevel_1_2);
+    firstlevel_1->appendRow(secondlevel_1_3);
+    firstlevel_1->appendRow(secondlevel_1_4);
+
+    firstlevel_2->appendRow(secondlevel_2_1);
+    firstlevel_2->appendRow(secondlevel_2_2);
+    firstlevel_2->appendRow(secondlevel_2_3);
+    firstlevel_2->appendRow(secondlevel_2_4);
+
+    firstlevel_3->appendRow(secondlevel_3_1);
+
+    firstlevel_4->appendRow(secondlevel_4_1);
+    firstlevel_4->appendRow(secondlevel_4_2);
+    firstlevel_4->appendRow(secondlevel_4_3);
+    firstlevel_4->appendRow(secondlevel_4_4);
+    firstlevel_4->appendRow(secondlevel_4_5);
+
+    ui->treeView->setModel(model);
+    ui->treeView->show();
+    ui->treeView->expandAll();
+}
+
+
+void MainWindow::caught_select()
+{
+    QObject::connect(ui->treeView, &QTreeView::clicked, [=] (const QModelIndex &index) {
+        QString text = index.data().toString();
+        select = text.toStdString();
+        //ui->textEdit->setText(QString::number(M[select]));
+    });
+
+}
+
+
+vector<string> MainWindow::open_file(const string &time)
+{
+    const string path = "E:\\学校\\气象\\2017信息工程\\气象信息系统工程课程实验\\实验材料\\样本数据\\data\\";
+    string file_name = fbegin + time + fend;
+    string name = path + file_name;
+    vector<string> data;
+    string word;
+    ifstream fs(name);
+    if(fs)
+        while (fs >> word)
+            data.push_back(word);
+    return data;
+}
+
+void MainWindow::show_text()
+{
+    QDateTime time1 = ui->dateTimeEdit->dateTime();
+    QDateTime time2 = ui->dateTimeEdit_2->dateTime();
+    QString str = time1.toString("yyyy-mm-dd hh:mm");
+    QString str2 = time2.toString("yyyy-mm-dd hh:mm");
+//    string str3 = time1.toString("yyyy-mm-dd hh:mm").toStdString();
+    QString out = "ploting pic from " + str + " to " +str2;
+    ui->textEdit->setText(out);
+}
+
+vector<float> MainWindow::get_data()
+{
+    vector<float> data;
+    QDateTime time = ui->dateTimeEdit->dateTime();
+    while (time <= ui->dateTimeEdit_2->dateTime()){
+        string name = time.toString("mmss").toStdString();
+        vector<string> D = open_file(name);
+        // modify
+        try {
+            data.push_back(stof(D[M[select]]));
+        } catch (exception) {
+            data.push_back(0);
+        }
+
+        time = time.addSecs(60);
+    }
+    return data;
+}
+
+void MainWindow::show_pics()
+{
+    QChart *chart = new QChart();
+    ui->graphicsView->setChart(chart);
+
+    // creat series
+    QLineSeries *series = new QLineSeries();
+
+    vector<float> data = get_data();
+    int index = 0;
+    QDateTime time = ui->dateTimeEdit->dateTime();
+    while (time <= ui->dateTimeEdit_2->dateTime()){
+        double timeD = time.toString("mm").toDouble();
+        series->append(timeD, data[index]);
+        time = time.addSecs(60);
+        index++;
+    }
+
+    chart->addSeries(series);
+    QValueAxis *axisX = new QValueAxis;
+    axisX->setRange(stoi(ui->dateTimeEdit->dateTime().toString("mm").toStdString()), stoi(ui->dateTimeEdit_2->dateTime().toString("mm").toStdString()));
+    axisX->setMinorTickCount(2);
+    axisX->setTitleText("time/min");
+
+    QValueAxis *axisY = new QValueAxis;
+    axisY->setMinorTickCount(2);
+    axisY->setTitleText(QString::fromStdString(V[select]));
+
+    //为序列设置坐标轴
+    chart->setAxisX(axisX, series);
+    chart->setAxisY(axisY, series);
+}
+
+void MainWindow::plot()
+{
+    show_text();
+    show_pics();
+}
---
+++ mainwindow.h
@@ -0,0 +1,65 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QWidget>
+#include <QDateTime>
+#include <vector>
+#include <string>
+#include <QString>
+#include <QTreeView>
+#include <QStandardItem>
+#include <QStandardItemModel>
+#include <map>
+#include <fstream>
+#include <QtCharts>
+#include <exception>
+
+using std::vector;
+using std::string;
+using std::map;
+using std::ifstream;
+using std::stof;
+using std::stoi;
+using std::exception;
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    void show_text();
+    vector<float> get_data();
+    void creat_tree();
+    void caught_select();
+    vector<string> open_file(const string &time);
+    void show_pics();
+
+    const string fbegin = "Z_SURF_I_00016-REG_2012121816";
+    const string fend = "_O_AWS-RD_FTM.txt";
+    string select;
+    map<string, int> M = {{"cur_temp",7},{"cur_dew_point_tem",12},{"cur_rel_hum",13},{"cur_water_vapor_pre",16},
+                          {"cur_wind_dire",27},{"cur_wind_speed",28},{"max_wind_speed/min",33},{"dire_max_wind_speed/min",32},
+                          {"cur_road_tem",35},{"cur_snow_th",47},{"cur_water_th",48},{"cur_ice_th",49},{"cur_free_tem",50},
+                          {"cur_snow_mel_con",51}};
+
+    map<string, string> V = {{"cur_temp","0.1°C"},{"cur_dew_point_tem","0.1°C"},{"cur_rel_hum","1%"},{"cur_water_vapor_pre","0.1hPa"},
+                          {"cur_wind_dire","1°"},{"cur_wind_speed","0.1m/s"},{"max_wind_speed/min","0.1m/s"},{"dire_max_wind_speed/min","1°"},
+                          {"cur_road_tem","0.1°C"},{"cur_snow_th","1mm"},{"cur_water_th","0.1mm"},{"cur_ice_th","0.1mm"},{"cur_free_tem","0.1°C"},
+                          {"cur_snow_mel_con","1%"}};
+
+
+
+private slots:
+    void plot();
+
+private:
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
